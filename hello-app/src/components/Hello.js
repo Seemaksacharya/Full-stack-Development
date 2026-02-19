690992f03701b77f260d45699fe028019a954567
Seema K S <seemaksacharya2@gmail.com>
2026-02-19 20:11:14 +0530

    update

---
+++ hello-app/src/components/Hello.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Hello(){
+return (
+    <div>
+        <h1>Hello World</h1>
+        <p>This is my React page.</p>
+    </div>
+);
+}
+
+export default Hello;
