1cca21055e4f132cde871b0dc853ed48df9d6cc5
Seema K S <seemaksacharya2@gmail.com>
2026-03-11 11:56:05 +0530

    update

---
+++ javascript-fundamentals/script.js
@@ -0,0 +1,19 @@
+//Var
+
+console.log(x);
+
+function test(){
+    var a=5;
+    console.log(a);
+}
+
+ if(true){
+    var num= 20;
+ }
+ console.log(num);
+
+ //let
+ if (true) {
+    let x =10;
+ }
+ console.log(x);
