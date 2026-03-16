0db7092bf525b78a1328be8c736261e94ddbe763
Seema K S <seemaksacharya2@gmail.com>
2026-03-16 19:39:14 +0530

    update

--- javascript-fundamentals/script.js
+++ javascript-fundamentals/script.js
@@ -1,4 +1,4 @@
-//Var
+
 
 
  if(true){
@@ -17,26 +17,19 @@
  console.log(pi);
 
 //Hoisting with var
-console.log(a);//Undefined
+console.log(a);
 var a = 10;
 
 
-const name ="Seema";
+
 const city= "Tth";
 let age=23;
-console.log(name);
 console.log(age);
 console.log(city);
 
 let x = 5;
  x = 10;
 console.log(x);
-/*
-// casing convention
-getElementByID //camelCasing
-GetType //PascalCasing
-array_push//snakecasing
-MAX_VALUE//SCREAMING_SNAKE_CASING
 
 //variable scope
  
@@ -45,13 +38,16 @@ var a=10;
 let b=20;
 const c=100;
 
-//function scope
+
 function test(){
     var a=5;
     a=10;
     console.log(a);
 }
-
+function test(){
+   var a=19;
+   console.log(a);
+}
 test();
 
-*/
+
