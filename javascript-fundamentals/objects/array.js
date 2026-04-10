c8e95dacf52aaee665493ff0af4e87da33458408
Seema K S <seemaksacharya2@gmail.com>
2026-04-10 14:08:26 +0530

    update

--- javascript-fundamentals/objects/array.js
+++ javascript-fundamentals/objects/array.js
@@ -1,5 +1,27 @@
-let fruits = ["apple","babana","mango"];
-console.log(fruits[0]);
-console.log(fruits.length);
-fruits.push("kiwi");
+let arr = [1, "hello", true];
 
+let fruits = ["apple","babana","mango"];
+console.log(fruits);
+
+console.log(fruits[0]);
+
+console.log(fruits.length);
+
+fruits.push("kiwi");
+console.log(fruits);
+
+fruits.unshift("pineapple");
+console.log(fruits);
+
+fruits.pop();
+console.log(fruits);
+
+fruits.shift();
+console.log(fruits);
+
+fruits[1] = "grapes";
+console.log(fruits);
+
+console.log(fruits.includes("orange"));
+
+console.log(fruits.indexOf("mango"));
