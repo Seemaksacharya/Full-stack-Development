3a9cf2af6381663a0e909b12ec940c3d1e214bfc
Seema K S <seemaksacharya2@gmail.com>
2026-06-10 19:52:41 +0530

    update

--- javascript-fundamentals/dom/script.js
+++ javascript-fundamentals/dom/script.js
@@ -4,5 +4,12 @@ console.log(box);
 let Element = document.querySelector("box")
 console.log(box);
 
+const person = {
+  name: "Seema",
+  greet: function() {
+    console.log(this.name);
+  }
+};
 
+person.greet();
 
